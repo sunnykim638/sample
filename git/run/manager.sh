27fdc4a220fd31be8a3d0d11f731d2eeb741e1a3
sunnykim638 <skim638@asu.edu>
2025-09-28 11:23:43 -0700

    update runs

--- git/run/manager.sh
+++ git/run/manager.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-python3 manager/manager.py --host 0.0.0.0 --mport "$1"
+python3 manager/manager.py --mport "$1"
--- git/run/user.sh
+++ git/run/user.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-python3 user.py --name "$1" --manager-ip "$2" --manager-port "$3" --my-ip "$4" --mport "$5" --cport "$6"
+python3 user.py --name "$1" --manager-ip "$2" --manager-port "$3" --mport "$4" --cport "$5"
